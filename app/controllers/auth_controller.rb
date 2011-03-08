2443cb3501c1300d8964795788a26ed3693eb021
Steven Vetzal <steve@vetzal.com>
2011-03-07 20:29:02 -0500

    removing authenticity token requirement

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,7 +1,5 @@
 class AuthController < ApplicationController
 
-  protect_from_forgery :except => :authenticate
-
   def authenticate
   end
 
--- app/controllers/sample_controller.rb
+++ app/controllers/sample_controller.rb
@@ -1,6 +1,8 @@
 class SampleController < ApplicationController
   include FacebookAuthFunctions
 
+  protect_from_forgery :except => [ :page1, :page2 ]
+
   before_filter :require_facebook_auth
 
 end
